87c54b1e43990d7e17eaa300134e9adcaa9177af
Hugh <hugh128ord@gmail.com>
2024-10-31 03:08:29 -0600

    Creado estructura de menu de navegacion

--- app/src/main/java/gt/com/burbujas/MainActivity.kt
+++ app/src/main/java/gt/com/burbujas/MainActivity.kt
@@ -1,17 +1,34 @@
 package gt.com.burbujas
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toggleButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        toggleButton = findViewById(R.id.buttonDrawerToggle)
+
+        toggleButton.setOnClickListener {
+            when {
+                drawerLayout.isDrawerOpen(GravityCompat.START) -> drawerLayout.closeDrawer(GravityCompat.START)
+                else -> drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(drawerLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
